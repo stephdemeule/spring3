ea127c2faf6b5ef8a0bcd7213e3a27335e9ba57a
stephdemeule <s.demeulemeester@campus-igs-toulouse.fr>
2022-12-10 18:02:36 +0100

    ch4-66

---
+++ src/main/java/projet/spring3/mi/FormatMessageReplacer.java
@@ -0,0 +1,45 @@
+package projet.spring3.mi;
+
+import org.springframework.beans.factory.support.MethodReplacer;
+
+import java.lang.reflect.Method;
+
+public class FormatMessageReplacer implements MethodReplacer {
+    public Object reimplement(Object arg0, Method method, Object[] args)
+            throws Throwable {
+
+        if (isFormatMessageMethod(method)) {
+
+            String msg = (String) args[0];
+
+            return "<h2>" + msg + "</h2>";
+        } else {
+            throw new IllegalArgumentException("Unable to reimplement method "
+                    + method.getName());
+        }
+    }
+    private boolean isFormatMessageMethod(Method method) {
+
+        // check correct number of params
+        if (method.getParameterTypes().length != 1) {
+            return false;
+        }
+
+        // check method name
+        if (!("formatMessage".equals(method.getName()))) {
+            return false;
+        }
+
+        // check return type
+        if (method.getReturnType() != String.class) {
+            return false;
+        }
+
+        // check parameter type is correct
+        if (method.getParameterTypes()[0] != String.class) {
+            return false;
+        }
+
+        return true;
+    }
+}
